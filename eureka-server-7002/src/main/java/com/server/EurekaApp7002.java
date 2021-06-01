23a7fffd023dcc7daae12cf131eb63093b532e6a
fyz123 <1173906808@qq.com>
2021-06-01 17:09:26 +0800

    新增注册中心集群

--- eureka-server-7002/src/main/java/com/server/EurekaApp7002.java
+++ eureka-server-7002/src/main/java/com/server/EurekaApp7002.java
@@ -1,13 +1,15 @@
 package com.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApp {
+public class EurekaApp7002 {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApp.class);
+        System.out.println("11");
+        SpringApplication.run(EurekaApp7002.class);
     }
 }

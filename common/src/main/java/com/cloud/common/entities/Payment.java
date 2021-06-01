9aac7673b29d2a09fab6faecd2dc806b453d30e1
fyz123 <1173906808@qq.com>
2021-06-01 15:54:03 +0800

    first commit

---
+++ common/src/main/java/com/cloud/common/entities/Payment.java
@@ -0,0 +1,9 @@
+package com.cloud.common.entities;
+
+import lombok.Data;
+
+@Data
+public class Payment {
+    private long id;
+    private String mSerial;
+}
---
+++ cosumer80/src/main/java/com/cloud/consumer/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.cloud.consumer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public RestTemplate getTemplate(){
+        return new RestTemplate();
+    }
+}

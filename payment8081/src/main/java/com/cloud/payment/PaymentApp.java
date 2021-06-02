3eda64e274f4f2d64055f43a76c7254631f2202e
fyz123 <1173906808@qq.com>
2021-06-02 20:16:40 +0800

    autuator微服务信息完善

--- payment8081/src/main/java/com/cloud/payment/PaymentApp.java
+++ payment8081/src/main/java/com/cloud/payment/PaymentApp.java
@@ -3,11 +3,13 @@ package com.cloud.payment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @MapperScan("com.cloud.payment.dao")
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentApp {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApp.class);
--- payment8081/src/main/java/com/cloud/payment/controllers/PaymentController.java
+++ payment8081/src/main/java/com/cloud/payment/controllers/PaymentController.java
@@ -3,16 +3,28 @@ package com.cloud.payment.controllers;
 import com.cloud.common.entities.CommonResult;
 import com.cloud.common.entities.Payment;
 import com.cloud.payment.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
+@Slf4j
 public class PaymentController {
+
+    private Log log = LogFactory.getLog(PaymentController.class);
 
     @Value("${server.port}")
     private String port;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Resource
     private PaymentService paymentService;
@@ -25,6 +37,21 @@ public class PaymentController {
     @PostMapping("/payment/save")
     public CommonResult save(@RequestBody Payment payment){
         return new CommonResult(200,"新增成功",paymentService.save(payment));
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object doDiscovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("serviceName: "+ service);
+        }
+
+        List<ServiceInstance> payment = discoveryClient.getInstances("PAYMENT");
+        for (ServiceInstance instance : payment) {
+            log.info("instance: "+ instance.getHost()+"\t"+instance.getPort()+"\t"+
+                    instance.getUri());
+        }
+        return discoveryClient;
     }
 
 }

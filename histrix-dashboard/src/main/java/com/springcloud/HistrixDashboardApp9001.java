07618548fbadf81f80276d7089f138bf4d065909
fyz123 <1173906808@qq.com>
2021-06-09 21:14:08 +0800

    监控页面

---
+++ histrix-dashboard/src/main/java/com/springcloud/HistrixDashboardApp9001.java
@@ -0,0 +1,36 @@
+package com.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableEurekaClient
+@Configuration
+public class HistrixDashboardApp9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HistrixDashboardApp9001.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean regBean = new ServletRegistrationBean(streamServlet);
+        regBean.setLoadOnStartup(1);
+        List mappingList = new ArrayList();
+        mappingList.add("/hystrix.stream");
+        regBean.setUrlMappings(mappingList);
+        regBean.setName("hystrixServlet");
+        return regBean;
+    }
+
+}

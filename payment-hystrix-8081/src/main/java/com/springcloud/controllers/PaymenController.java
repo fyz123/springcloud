cccdff447a44bcc9a03c7c2deb4e3e89d08816ab
fyz123 <1173906808@qq.com>
2021-06-09 20:31:56 +0800

    豪猪哥熔断

--- payment-hystrix-8081/src/main/java/com/springcloud/controllers/PaymenController.java
+++ payment-hystrix-8081/src/main/java/com/springcloud/controllers/PaymenController.java
@@ -1,9 +1,11 @@
 package com.springcloud.controllers;
 
+import cn.hutool.core.date.DateUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.springcloud.services.PaymentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -29,6 +31,26 @@ public class PaymenController {
 
     public String getDeault(){
         return "payment端服务超时或异常，我来兜底！";
+    }
+
+
+    /** 测试熔断 */
+    @GetMapping("/payment/histrix/testDown/{id}")
+    @HystrixCommand(fallbackMethod = "downFallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+    })
+    public String testDown(@PathVariable int id){
+        if(id < 0){
+            throw new RuntimeException("入参不能小于0");
+        }
+        return "请求成功，"+ DateUtil.current();
+    }
+
+    public String downFallback(@PathVariable int id){
+        return "熔断期间的降级";
     }
 
 }

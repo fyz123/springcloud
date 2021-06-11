b0370804c8159aba139109529a175cca8823d30a
fyz123 <1173906808@qq.com>
2021-06-11 17:55:19 +0800

    监控页面

--- histrix-dashboard/src/main/java/com/springcloud/HistrixDashboardApp9001.java
+++ histrix-dashboard/src/main/java/com/springcloud/HistrixDashboardApp9001.java
@@ -15,22 +15,9 @@ import java.util.List;
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableEurekaClient
-@Configuration
 public class HistrixDashboardApp9001 {
     public static void main(String[] args) {
         SpringApplication.run(HistrixDashboardApp9001.class);
-    }
-
-    @Bean
-    public ServletRegistrationBean getServlet() {
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean regBean = new ServletRegistrationBean(streamServlet);
-        regBean.setLoadOnStartup(1);
-        List mappingList = new ArrayList();
-        mappingList.add("/hystrix.stream");
-        regBean.setUrlMappings(mappingList);
-        regBean.setName("hystrixServlet");
-        return regBean;
     }
 
 }
--- payment-hystrix-8081/src/main/java/com/springcloud/PaymentHistrix8081.java
+++ payment-hystrix-8081/src/main/java/com/springcloud/PaymentHistrix8081.java
@@ -1,15 +1,36 @@
 package com.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@Configuration
 public class PaymentHistrix8081 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHistrix8081.class);
     }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean regBean = new ServletRegistrationBean(streamServlet);
+        regBean.setLoadOnStartup(1);
+        List mappingList = new ArrayList();
+        mappingList.add("/hystrix.stream");
+        regBean.setUrlMappings(mappingList);
+        regBean.setName("hystrixServlet");
+        return regBean;
+    }
+
 }
